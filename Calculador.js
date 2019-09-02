0ae5422a5cf0bea240be7aa7466428d1b279aa75
Fede <fedeericosanchez18@gmail.com>
2019-09-02 14:45:58 -0300

    inicial

---
+++ Calculador.js
@@ -0,0 +1,59 @@
+function init(){
+
+    var monto ;
+    var cuotas;
+    var cuotaMensual;
+    var montoTotal;
+    var calculo = document.getElementById("calcular");
+
+    //eventos
+
+    calculo.onclick = function(){
+        monto = document.getElementById("monto");
+        cuotas = document.getElementById("cuotas");
+        calcular(monto,cuota)
+
+    }
+
+
+    //funciones 
+    
+    function calcular(monto , cuotas){
+
+        if(monto = 3000){
+            if(cuotas = 2 ){
+                //cuota 1599,90
+                //total 3199,80
+
+            }
+            if(cuotas = 3 ){
+                //cuota 1099,90
+                //total 3299,70
+            }
+            if(cuotas = 4 ){
+                //cuota 849,90
+            }
+            if(cuotas = 5 ){
+                
+            }
+            if(cuotas = 6 ){
+                
+            }
+            if(cuotas = 8 ){
+                
+            }
+            if(cuotas = 10 ){
+                
+            }
+            
+            if(cuotas = 12 ){
+                
+            }
+            
+            
+            
+        
+
+    }
+}
+}
